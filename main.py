d684c6a258d8e5055af1eae86ae11e42ba5e61bf
thehiddenmonkey <gian.s.brar@gmail.com>
2020-02-13 17:23:06 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ while (breakLoop == False):
   if menu == "i":
     print("Programmed by Gian Brar for Trisha Reddy's 14th birthday, OVERKILL PRIVACY MACHINE is for the writer who wishes for others not to view anything they write. It utilizes military grade SHA (Secure Hash Algorithim) encryption with a program that has been proven mathematically impossible to reverse in order to hide your private info from your family.")
   elif menu == "p":
-    passwordMenu = input("M: Make password\nD: Delete password")
+    passwordMenu = input("M: Make password\nD: Delete password\nG: Get password key")
     passwordMenu = passwordMenu.lower()
     if passwordMenu == "m":
     # sLevel = input("Please choose level of security.\nBASIC: Simple encryption; medium security level.\nADVANCED: More complex encryption which may take longer for larger inputs.\nGOD: Incredibly complicated encryption which may take very long time to en/decrypt.")
     # sLevel = sLevel.lower()
       passwordI = input("Please enter password.")
-      passwordIE = hashlib.sha3_512(password.encode("utf-8")).hexdigest()
+      passwordIE = hashlib.sha3_512(passwordI.encode("utf-8")).hexdigest()
       passWriteFile.write(passwordIE)
       passWriteFile.write("\n")
       print(passwordI + " was ENCRYPTED into password list.")
     elif passwordMenu == "d":
-      deletePassword = input("Choose password to be deleted:\n")
+      deletePassword = input("Choose password to be deleted:\n" + password)
+    elif passwordMenu == "g":
+      getPassword = input("Enter password for key generation; password will NOT be added to list.")
+      print(hashlib.sha3_512(getPassword.encode("utf-8")).hexdigest())
   else:
     print("Please select a listed choice.")
