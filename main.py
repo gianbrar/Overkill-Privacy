2a50f48a6849126bc21b4fa6855959ee513b8369
thehiddenmonkey <gian.s.brar@gmail.com>
2020-02-14 19:01:12 -0500

    Update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import hashlib
 import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 pwdPath = "/bin/OverkillPrivacy/pwdf.txt"
 pwdFile = open(pwdPath, 'r')
 pwd = pwdFile.read()
 os.system("cd " + pwd)
-os.system("./sudocheck.bash")
+os.system("./sudocheck")
 
 breakLoop = False
 passPath = "/bin/OverkillPrivacy/pass.txt"
@@ -19,12 +21,12 @@ storyWriteFile = open(storyPath, 'w')
 
 
 while (breakLoop == False):
-  menu = input("OVERKILL PRIVACY MACHINE 1.0\nP: Password menu\nE: Encrypt\nD: Decrypt\nI: Info")
+  menu = input("OVERKILL PRIVACY MACHINE 1.0\nP: Password menu\nE: Encrypt\nD: Decrypt\nI: Info\n")
   menu = menu.lower()
   if menu == "i":
     print("Programmed by Gian Brar for Trisha Reddy's 14th birthday, OVERKILL PRIVACY MACHINE is for the writer who wishes for others not to view anything they write. It utilizes military grade SHA (Secure Hash Algorithim) encryption with a program that has been proven mathematically impossible to reverse in order to hide your private info from your family.")
   elif menu == "p":
-    passwordMenu = input("M: Make password\nD: Delete password\nG: Get password key")
+    passwordMenu = input("M: Make password\nD: Delete password\nG: Get password key\n")
     passwordMenu = passwordMenu.lower()
     if passwordMenu == "m":
     # sLevel = input("Please choose level of security.\nBASIC: Simple encryption; medium security level.\nADVANCED: More complex encryption which may take longer for larger inputs.\nGOD: Incredibly complicated encryption which may take very long time to en/decrypt.")
@@ -40,7 +42,7 @@ while (breakLoop == False):
       getPassword = input("Enter password for key generation testing; password will NOT be added to list.")
       print(hashlib.sha3_512(getPassword.encode("utf-8")).hexdigest())
   elif menu == "e":
-    encryptMenu = input("T: Type out story\nG: Encrypt from Google Doc")
+    encryptMenu = input("T: Type out story\nG: Encrypt from Google Doc\n")
     encryptMenu = encryptMenu.lower()
     if encryptMenu == "t":
       encryptName = input("Please give name of story.")
@@ -48,6 +50,14 @@ while (breakLoop == False):
       if encryptNameBool == True:
         os.system("vim -c 'startinsert' " + encryptName)
       else:
-        os.system("vim -c 'startinstert' " + encryptName + ".txt")
+        os.system("vim -c 'startinsert' " + encryptName + ".txt")
+    elif encryptMenu == "g":
+      gauth = GoogleAuth()
+      gauth.LocalWebserverAuth()
+      drive = GoogleDrive(gauth)
+      file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+      selection = input("Please choose Google Doc Name.")
+      file1 = drive.CreateFile({'id': file1['id']})
+      file1.GetContentFile(selection)
   else:
     print("Please select a listed choice.")
